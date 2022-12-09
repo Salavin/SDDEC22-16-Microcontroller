c07c1d53c4dd63cf615ae8feec8fe474c08becbe
Sam Lavin <salavin12@gmail.com>
2022-12-09 09:03:44 -0600

    Final code

--- fftTest.ino
+++ fftTest.ino
@@ -8,14 +8,13 @@ int lookupNote(int str, int fret);
 int findHighestNote(int notes[6]);
 void findFretAvg(int str);
 
-
 const int strE = A0;
 const int strA = A1;
 const int strD = A2;
 const int strG = A3;
 const int strB = A4;
 const int strEe = A5; 
-const int stringVals[6] = {strE, strA, strD, strB, strEe};
+const int stringVals[6] = {strE, strA, strD, strG, strB, strEe};
 
 const int BOTTOM_STRING_TO_READ = 0;
 const int TOP_STRING_TO_READ = 6;
@@ -35,6 +34,9 @@ int strumAvg = 0;
 int zeroPoint = 2035;   // Reassigned in setup(), used in waitForStrumDecay()
 int decayThresh = 2100; // Reassigned in setup(), used in waitForStrumDecay()
 int attackThresh = 2100; //Reassigned in setup(), used in loop() to determine when a string has been plucked
+int currentNote = 0;
+int maxFret = 0;
+int currentString = 0;
 
 int offset[6] = {16, 21, 26, 31, 35, 40};
 
@@ -44,7 +46,7 @@ void setup()
   analogReadResolution(ANALOG_READ_RESOLUTION); //12-bit mode
 
   //These next few lines determines what the "zero point" will be for the ADC input used for strum detection
-  //Make sure guitar is silenced when powering on microcontroller
+  //Make sure guitar is silenced when powering on microcontroller!
   int i = 0;
   int sum = 0;
   delay(100);
@@ -53,66 +55,55 @@ void setup()
     zeroPoint = sum/i;
     i++;
   }
-  decayThresh = zeroPoint + 10;
-  attackThresh = zeroPoint + 150;
+  decayThresh = zeroPoint + 20;
+  attackThresh = zeroPoint + 125;
 }
 
 void loop()
 {
-  int newNotes[NUM_STRINGS] = {};    //MIDI notes associated with each string's current fret value
-  bool noteChanged[6] = {false, false, false, false, false, false};
+  maxFret = 0;        //resets for each loop - used for "currentNote"
+  currentString = 0;  //resets for each loop - used for "currentNote"
 
   //wait for strum
   while (strum < attackThresh)
   {
+    receiveAndSetConfig();
     strum = analogRead(A7);
   }
 
-  for (int i = BOTTOM_STRING_TO_READ; i < NUM_STRINGS; i++)
+  //Read strings
+  for (int i = 0; i < 6; i++)
   {
     strings[i] = adcReadAvg(stringVals[i], nAvg);
     frets[i] = lookupFret(i, strings[i]);
-    newNotes[i] = lookupNote(i, frets[i]);    
 
-    if (newNotes[i] != lookupNote(i, 0))
-    {
-      noteChanged[i] = true;
-      noteOn(0, newNotes[i], 100);
-      notes[i] = newNotes[i];
-      delay(1);
-    }
     Serial.print(frets[i]);
     Serial.print("\t");
   }
 
-  // Serial.print("Notes: ");
-  // for (int i = BOTTOM_STRING_TO_READ; i < NUM_STRINGS; i++)
-  // {
-  //   Serial.print(notes[i]);
-  //   Serial.print("\t");
-  // }
-  // Serial.print("\n");
-  // Serial.println(strum);
-
-  //nextNote = findHighestNote(notes[]);
-
-  // wait for strum button to be released before continuing so only one note is sent per button press
-  waitForStrumDecay(nStrAvg);
-  strum = 0;
-  
-  for (int i = BOTTOM_STRING_TO_READ; i < NUM_STRINGS; i++)
-  {
-    if (noteChanged[i])
+  //Highest *fret* (not necessarialy highest *note*) becomes current note to be sent
+  for(int i = BOTTOM_STRING_TO_READ; i < NUM_STRINGS; i++){
+    if (frets[i] > maxFret)
     {
-      noteOff(0, notes[i], 100);
+      maxFret = frets[i];
+      currentString = i;
     }
   }
+  currentNote = lookupNote(currentString, maxFret);
+
+  //Send note
+  noteOn(0, currentNote, 100);
+  Serial.print("Note: ");
+  Serial.println(currentNote);
+
+  //Wait for note to decay, then turn off
+  waitForStrumDecay(nStrAvg);
+  strum = 0;
+  noteOff(0, currentNote, 100);  
   
   //Check for new Midi config and set config 
-  receiveAndSetConfig();
-
-  delay(1);
-  Serial.println("off");
+  //receiveAndSetConfig();
+  delay(1); //this delay might not be needed anymore
 }
 
 /***************************************************
@@ -153,24 +144,41 @@ void waitForStrumDecay(int nStrAvgMov){
 
 
 void receiveAndSetConfig(){
-  int SOM = 60;
-  int EOM = 50;
+  int SOM = 6;
+  int EOM = 5;
   midiEventPacket_t general = MidiUSB.read();
   
   if(general.byte2 == SOM)
   {
+    Serial.print("Uploading config...");
     int iter = 0; //offset list iterator
+    int dupIter = 0;
+    int seen[30]; 
     midiEventPacket_t data;
     while(true)
-    {        
+    {       
+
       midiEventPacket_t data = MidiUSB.read();
-      if(data.byte2 == EOM)  break;
+
+ 
+      if(data.byte2 == EOM) {
+        break;
+      }
       else if(data.byte2 != 0)
       {
-        offset[iter++] = data.byte2;
+        
+        if(offset[iter-1] != data.byte2 && data.byte2 != SOM && data.byte2 != EOM) {
+
+          offset[iter++] = data.byte2;
+        }
+        
+        seen[dupIter++] == data.byte2;
       }
+
     }
+    Serial.print("Done loading config");
   }
+  
 }
 
 int adcReadAvg(int port, int nAvg){
@@ -196,22 +204,22 @@ int adcReadAvg(int port, int nAvg){
 
 int lookupFret(int string, int adcVal) 
 {
-    if (adcVal < 200) return 0;
-    else if (adcVal >= 200 && adcVal <= 274) return 1;
-    else if (adcVal >= 275 && adcVal <= 299) return 2;
-    else if (adcVal >= 300 && adcVal <= 599) return 3;
-    else if (adcVal >= 800 && adcVal <= 899) return 4;
-    else if (adcVal >= 1100 && adcVal <= 1199) return 5;
-    else if (adcVal >= 1300 && adcVal <= 1499) return 6;
-    else if (adcVal >= 1600 && adcVal <= 1799) return 7;
-    else if (adcVal >= 1800 && adcVal <= 2199) return 8;
-    else if (adcVal >= 2300 && adcVal <= 2399) return 9;
-    else if (adcVal >= 2400 && adcVal <= 2699) return 10;
-    else if (adcVal >= 2600 && adcVal <= 2924) return 11;
-    else if (adcVal >= 2925 && adcVal <= 3214) return 12;
-    else if (adcVal >= 3215 && adcVal <= 3399) return 13;
-    else if (adcVal >= 3500 && adcVal <= 3799) return 14;
-    else if (adcVal >= 3800) return 15;
+    if (adcVal < 200) return 0;                           //Average Values:
+    else if (adcVal >= 200 && adcVal <= 274) return 1;    //264
+    else if (adcVal >= 275 && adcVal <= 295) return 2;    //284
+    else if (adcVal >= 296 && adcVal <= 699) return 3;    //576 E-B, e switches between 306 and 576
+    else if (adcVal >= 700 && adcVal <= 999) return 4;    //867
+    else if (adcVal >= 1000 && adcVal <= 1300) return 5;  //1157, sometimes 1350 on low E
+    else if (adcVal >= 1301 && adcVal <= 1635) return 6;  //1448, 1621, sometimes 1350 on low E (more often than on 5)
+    else if (adcVal >= 1636 && adcVal <= 1900) return 7;  //1737, 1891
+    else if (adcVal >= 1901 && adcVal <= 2100) return 8;  //2029, 2164 on low E
+    else if (adcVal >= 2101 && adcVal <= 2399) return 9;  //2164 (more often than 8), 2322
+    else if (adcVal >= 2400 && adcVal <= 2699) return 10; //2438, 2615
+    else if (adcVal >= 2700 && adcVal <= 2924) return 11; //2819, 2909, 2715, 2985
+    else if (adcVal >= 2925 && adcVal <= 3220) return 12; //3059, 3206
+    else if (adcVal >= 3221 && adcVal <= 3510) return 13; //3342, 3501, 3262
+    else if (adcVal >= 3511 && adcVal <= 3950) return 14; //3341, 3682, 3799, 3818
+    else if (adcVal >= 3951) return 15;                   //4095
     else return 0;
 }
 
@@ -226,19 +234,19 @@ int lookupNote (int str, int fret)
       note = fret + offset[0];  // E
       break;
     case 1: 
-      note = fret + offset[0];
+      note = fret + offset[1];  //A
       break;
     case 2:
-      note = fret + offset[0];  // D
+      note = fret + offset[2];  // D
       break;
     case 3:
-      note = fret + offset[0];  // G
+      note = fret + offset[3];  // G
       break;
     case 4:
-      note = fret + offset[0];  // B
+      note = fret + offset[4];  // B
       break;
     case 5:
-      note = fret + offset[0];  // e
+      note = fret + offset[5];  // e
       break;
     default:
       return 0;
@@ -275,7 +283,7 @@ int findHighestNote(int notes[6])
 }
 
 
-
+//This function is for building lookup tables. Not needed for actual operation of guitar.
 void findFretAvg(int str){
   int onOff = 0;
   int sum = 0;
